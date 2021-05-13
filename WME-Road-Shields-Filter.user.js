818c46e12c2bc75b804993de8b01b1166eac2c15
Ian "The_Cre8r" Harshbarger <TheCre8r@users.noreply.github.com>
2021-05-12 22:14:32 -0400

    Update WME-Road-Shields-Filter.user.js

--- WME-Road-Shields-Filter.user.js
+++ WME-Road-Shields-Filter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Road Shield Filter
 // @namespace    https://github.com/thecre8r/
-// @version      2021.05.09.03
+// @version      2021.05.12.01
 // @description  Observes for the modal
 // @include      https://www.waze.com/editor*
 // @include      https://www.waze.com/*/editor*
@@ -18,66 +18,13 @@
 /* global $ */
 /* global W */
 /* global WazeWrap */
-/* global getState */
 
 
 (function() {
 
-    var getState;
 
     function abbrState(input, to){
-        var states = [
-            ['Arizona', 'AZ'],
-            ['Alabama', 'AL'],
-            ['Alaska', 'AK'],
-            ['Arkansas', 'AR'],
-            ['California', 'CA'],
-            ['Colorado', 'CO'],
-            ['Connecticut', 'CT'],
-            ['Delaware', 'DE'],
-            ['Florida', 'FL'],
-            ['Georgia', 'GA'],
-            ['Hawaii', 'HI'],
-            ['Idaho', 'ID'],
-            ['Illinois', 'IL'],
-            ['Indiana', 'IN'],
-            ['Iowa', 'IA'],
-            ['Kansas', 'KS'],
-            ['Kentucky', 'KY'],
-            ['Louisiana', 'LA'],
-            ['Maine', 'ME'],
-            ['Maryland', 'MD'],
-            ['Massachusetts', 'MA'],
-            ['Michigan', 'MI'],
-            ['Minnesota', 'MN'],
-            ['Mississippi', 'MS'],
-            ['Missouri', 'MO'],
-            ['Montana', 'MT'],
-            ['Nebraska', 'NE'],
-            ['Nevada', 'NV'],
-            ['New Hampshire', 'NH'],
-            ['New Jersey', 'NJ'],
-            ['New Mexico', 'NM'],
-            ['New York', 'NY'],
-            ['North Carolina', 'NC'],
-            ['North Dakota', 'ND'],
-            ['Ohio', 'OH'],
-            ['Oklahoma', 'OK'],
-            ['Oregon', 'OR'],
-            ['Pennsylvania', 'PA'],
-            ['Rhode Island', 'RI'],
-            ['South Carolina', 'SC'],
-            ['South Dakota', 'SD'],
-            ['Tennessee', 'TN'],
-            ['Texas', 'TX'],
-            ['Utah', 'UT'],
-            ['Vermont', 'VT'],
-            ['Virginia', 'VA'],
-            ['Washington', 'WA'],
-            ['West Virginia', 'WV'],
-            ['Wisconsin', 'WI'],
-            ['Wyoming', 'WY'],
-        ];
+        var states = [['Arizona', 'AZ'],['Alabama', 'AL'],['Alaska', 'AK'],['Arkansas', 'AR'],['California', 'CA'],['Colorado', 'CO'],['Connecticut', 'CT'],['Delaware', 'DE'],['Florida', 'FL'],['Georgia', 'GA'],['Hawaii', 'HI'],['Idaho', 'ID'],['Illinois', 'IL'],['Indiana', 'IN'],['Iowa', 'IA'],['Kansas', 'KS'],['Kentucky', 'KY'],['Louisiana', 'LA'],['Maine', 'ME'],['Maryland', 'MD'],['Massachusetts', 'MA'],['Michigan', 'MI'],['Minnesota', 'MN'],['Mississippi', 'MS'],['Missouri', 'MO'],['Montana', 'MT'],['Nebraska', 'NE'],['Nevada', 'NV'],['New Hampshire', 'NH'],['New Jersey', 'NJ'],['New Mexico', 'NM'],['New York', 'NY'],['North Carolina', 'NC'],['North Dakota', 'ND'],['Ohio', 'OH'],['Oklahoma', 'OK'],['Oregon', 'OR'],['Pennsylvania', 'PA'],['Rhode Island', 'RI'],['South Carolina', 'SC'],['South Dakota', 'SD'],['Tennessee', 'TN'],['Texas', 'TX'],['Utah', 'UT'],['Vermont', 'VT'],['Virginia', 'VA'],['Washington', 'WA'],['West Virginia', 'WV'],['Wisconsin', 'WI'],['Wyoming', 'WY'],];
 
         if (to == 'abbr'){
             input = input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
@@ -124,6 +71,31 @@
                 CreateError("Potential Error: Please Review");
             }
             //console.log(match[1])
+            function MakeStateShield(match,stateoverride){
+                console.log("State");
+                let State;
+                if (stateoverride) {
+                    State = stateoverride;
+                } else {
+                    State = abbrState(match[1], 'name')
+                }
+
+                if (document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="` + State + ` - State Main"]`) && match[3] == undefined) {
+                    console.log(match[1]);
+                    document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="` + State + ` - State Main"]`).click()
+                } else if (document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="` + State + ` - State ` + match[3] + `"]`)) {
+                    document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="` + State + ` - State ` + match[3] + `"]`).click()
+                } else if (document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="` + State + ` - State Main"]`) && match[3] !== undefined) {
+                    CreateError("Error: " + State +" - State " + match[3] + " Road Shield is not available" );
+                    console.log(match[1]);
+                    console.log(match[3]);
+                    return;
+                } else {
+                    console.log("Primary Identifier Not Found");
+                    return;
+                    //document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="SR generic Main"]`)
+                }
+            }
             switch (match[1] ) {
                 case "H":
                 case "I":
@@ -139,43 +111,39 @@
                             break;
                     }
                     break;
-                case "US":
-                    console.log("US");
-                    switch (match[3]) {
-                        case "BUS":
-                            console.log("Business");
-                            document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="US-# BUS"]`).click()
-                            break;
-                        case "BYP"://(BUS|ALT|BYP|CONN|SPUR|TRUCK)
-                            console.log("By-Pass");
-                            document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="US-# BYP"]`).click()
-                            break;
-                        case "ALT":
-                            console.log("Alternate");
-                            document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="US-# ALT"]`).click()
-                            break;
-                        default:
-                            console.log("Main");
-                            document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="US Hwy Main"]`).click()
-                            break;
-                    }
-                    break;
-                default:
-                    if (document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="`+abbrState(match[1], 'name')+` - State Main"]`) && match[3] == undefined) {
+                case "SR":
+                    if (getState() == "Pennsylvania") {
+                        MakeStateShield(match,"Pennsylvania");
+                    } else if (match[3] == undefined) {
                         console.log(match[1]);
-                        document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="`+abbrState(match[1], 'name')+` - State Main"]`).click()
-                    } else if (document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="`+abbrState(match[1], 'name')+` - State ` + match[3] + `"]`)) {
-                        document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="`+abbrState(match[1], 'name')+` - State ` + match[3] + `"]`).click()
-                    } else if (document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="`+abbrState(match[1], 'name')+` - State Main"]`) && match[3] !== undefined) {
-                        CreateError("Error: " + abbrState(match[1], 'name')+" - State Main Road Shield is not available" );
+                        document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="SR generic Main"]`).click()
+                    } else if (match[3] !== undefined) {
+                        CreateError("Error: SR " + match[3] + " Road Shield is not available" );
                         console.log(match[1]);
                         console.log(match[3]);
                         return;
                     } else {
-                        console.log("Primary Identifier Not Found");
+                        CreateError("Error: SR " + match[3] + " Road Shield is not available" );
+                        console.log(match[1]);
+                        console.log(match[3]);
                         return;
                         //document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="SR generic Main"]`)
                     }
+                    break;
+                case "US":
+                    console.log("US");
+                    if (match[3] == undefined) {
+                        document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="US Hwy Main"]`).click()
+                    } else if (document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="US-# ` + match[3] + `"]`)) {
+                        console.log(match[3]);
+                        document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="US-# ` + match[3] + `"]`).click()
+                    } else {
+                        CreateError("Error: US-# " + match[3] + " Road Shield is not available or does not parse" );
+                        return;
+                    }
+                    break;
+                default:
+                    MakeStateShield(match)
                     break;
             }
             if (match[2]) {
@@ -200,12 +168,38 @@
                     break;
                 default:
                     document.querySelector("#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(3) > wz-text-input").value = ""
-                    console.log("Primary Identifier Not Found");
-                    //document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > [title="SR generic Main"]`)
                     break;
             }
             document.querySelector("#wz-dialog-container > div > wz-dialog > wz-dialog-controls > wz-button.apply-button.hydrated").disabled = false
         };
+    }
+    function filterShields() {
+        let country = W.model.getTopCountry().name
+        let state = getState()
+        if (country == "Canada" || country == "United States") {
+            console.log("WME Road Shield Filter Filtered " + state)
+            for(var j = 1; j <= document.querySelector("#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu").childElementCount; j++){
+                let lineitem = document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > wz-menu-item:nth-child(`+j+`)`)
+                let iTxt = lineitem.innerText // the string to check against
+
+                let SearchStrings = ['Interstate Main','US Hwy','SR generic','CR generic','I-','US-','BIA','FSR','National',state]
+                let length = SearchStrings.length;
+                lineitem.hidden = true;
+                while(length--) {
+                    if (iTxt.indexOf(SearchStrings[length])!=-1) {
+                        console.log(iTxt.indexOf(SearchStrings[length]))
+                        console.log("WME Road Shield Filter Should Keep:" + iTxt)
+                        if (state == "Virginia" && iTxt.includes("West Virginia")) {
+                            //skip
+                        }
+                        else {
+                            lineitem.hidden = false;
+                        }
+                    }
+                }
+
+            }
+        }
     }
     function letsWatch() {
         let observer = new MutationObserver(mutations => {
@@ -217,49 +211,23 @@
                     if (document.querySelector("#wz-dialog-container > div > wz-dialog") && document.querySelector("#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu")) {
                         console.log("WME Road Shield Filter Ran")
                         RegexMatch()
-                        let country = W.model.getTopCountry().name
-                        let state = getState
-                        if (country == "United States") {
-                            console.log("WME Road Shield Filter Filtered " + state)
-                            for(var j = 1; j <= document.querySelector("#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu").childElementCount; j++){
-                                let lineitem = document.querySelector(`#wz-dialog-container > div > wz-dialog > wz-dialog-content > div:nth-child(1) > wz-menu > wz-menu-item:nth-child(`+j+`)`)
-                                let iTxt = lineitem.innerText // the string to check against
-
-                                let SearchStrings = ['Interstate Main','US Hwy','SR generic','CR generic','I-','US-','BIA','FSR',state]
-                                let length = SearchStrings.length;
-                                lineitem.hidden = true;
-                                while(length--) {
-                                    if (iTxt.indexOf(SearchStrings[length])!=-1) {
-                                        console.log(iTxt.indexOf(SearchStrings[length]))
-                                        console.log("WME Road Shield Filter Should Keep:" + iTxt)
-                                        if (state == "Virginia" && iTxt.includes("West Virginia")) {
-                                            //skip
-                                        }
-                                        else {
-                                            lineitem.hidden = false;
-                                        }
-                                    }
-                                }
-
-                            }
-                        }
+                        filterShields()
                     }
                 }
             });
         });
         observer.observe(document.getElementById('wz-dialog-container'), { childList: true });
     }
-    function gettingState() {
+    function getState() {
         if (W.selectionManager.getSelectedFeatures().length > 0) {
             let pStID = W.selectionManager._getSelectedSegments()[0].attributes.primaryStreetID;
-            getState = WazeWrap.Model.getStateName(pStID);
+            return WazeWrap.Model.getStateName(pStID);
         }
     }
 
     function bootstrap(tries = 1) {
         //log("bootstrap attempt "+ tries);
         if (W && W.map && W.model && WazeWrap.Ready) {
-            WazeWrap.Events.register("selectionchanged", null, gettingState);
             letsWatch();
         } else if (tries < 1000) {
             setTimeout(() => bootstrap(tries++), 200);
